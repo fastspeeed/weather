f41a177d3ce01c86731ea1a037cba29b11ff96e8
fastspeeed <fastspeeed@gmail.com>
2018-03-07 23:39:59 +0800

    add time control

--- index.js
+++ index.js
@@ -1,18 +1,24 @@
 const oracledb = require('oracledb');
 const fs = require('fs');
 const moment = require('moment');
-//let config=require('./app.json');
-//let running=require('./running.json');
+let config=require('./app.json');
+let running=require('./running.json');
 const { createLogger, format,transports } = require('winston');
 const parserJson = require("./lib/parser.js");
 
 const transform = require("./lib/transform.js");
 const removeEmpty =require("./lib/remove-empty.js");
-const Client = require('ftp');
 const { combine, timestamp,  prettyPrint } = format;
 const uuidv4 = require('uuid/v4');
 
+const convertBeijinUtcToStand=(str)=>moment(str,"YYYY-MM-DD HH:mm:ss").utcOffset(0)
+const convertStandToBeijing=(str)=>	moment(str,"YYYY-MM-DD HH:mm:ss").utcOffset(16*60)
+const momentToString=(d)=>d.format("YYYY-MM-DD HH:mm:ss");
+const stringToMoment=(str)=>moment(str,"YYYY-MM-DD HH:mm:ss");
+const getBeginDate=(interval=config.interval*60*1000)=>(logDate,nowDate)=>nowDate-logDate>interval|nowDate<=logDate?nowDate.subtract(interval/1000,'seconds'):logDate;
 
+
+//const beginTimer = getBeginDate()(logDate,moment()).format("YYYY-MM-DD HH:mm:ss")
 const logger = createLogger({
     level: 'info',
     format: combine(
@@ -20,20 +26,17 @@ const logger = createLogger({
         prettyPrint()
     ),
     transports: [
-        new transports.File({ filename: 'error.log', level: 'error' }),
-        //new transports.File({ filename: 'combined.log' })
+        new transports.File({ filename: 'error.log', level: 'info' }),
+        // new transports.File({ filename: 'combined.log' })
     ]
 });
-const getBeginDate=(interval=10*60*1000)=>(logDate,nowDate)=>nowDate-logDate>10*60*1000?nowDate:logDate;
 
+const Client = require('ftp');
 
-const beginTimer = getBeginDate()(logDate,moment()).format("YYYY-MM-DD HH:mm:ss")
-
-
-const ftpReady=(config,running)=>{
+const ftpReady=()=>{
     const ftp = new Client();
     ftp.on('ready', function() {
-        dbReady(ftp,config,running);
+        dbReady(ftp);
 /*        c.put('foo.txt', 'foo.remote-copy.txt', function(err) {
             if (err) throw err;
             c.end();
@@ -44,7 +47,7 @@ const ftpReady=(config,running)=>{
     })
     ftp.connect(config.ftp);
 }
-const dbReady=(ftp,config,running)=>{
+const dbReady=(ftp)=>{
     oracledb.fetchAsString = [ oracledb.CLOB ];
     oracledb.getConnection(
         config.db,
@@ -55,25 +58,29 @@ const dbReady=(ftp,config,running)=>{
                 ftp.end();
                 return;
             }
-            let logDate=moment(running.date,"YYYY-MM-DD HH:mm:ss")
-            const beginTimer = getBeginDate()(logDate,moment()).format("YYYY-MM-DD HH:mm:ss")
-            connection.execute(config.query,[beginTimer],
+            let logDate=stringToMoment(running.date);
+			let beginTimer=momentToString(getBeginDate()(logDate,moment()));
+			console.log(beginTimer);
+            const beginTimerStand = momentToString(convertBeijinUtcToStand(beginTimer));
+			console.log(beginTimerStand);
+            connection.execute(config.query,[beginTimerStand],
                 function(err, result) {
                     if (err) { logger.error({query:config.query,error:err.message}); doRelease(connection,ftp); return; }
                     if (result.rows.length === 0)
                         logger.info("No results");
                     else {
-                        logger.info(result.rows.length);
+                        logger.info(`record count=${result.rows.length}`);
                         createJson(result.rows,ftp)
                     }
-                    getMaxDate(connection,ftp,config,running)
+                    console.log('over')
+					getMaxDate(connection,ftp)
                    
                     
                 });
         });
 }
 
-const getMaxDate=(connection,ftp,config,running)=>{
+const getMaxDate=(connection,ftp)=>{
     connection.execute(config.maxDate,
         function(err, result) {
             if (err) { logger.error({query:config.maxDate,error:err.message}); doRelease(connection,ftp); return; }
@@ -81,9 +88,10 @@ const getMaxDate=(connection,ftp,config,running)=>{
                 logger.error({query:config.maxDate,error:'count must be 1'});
             else {
 				let maxdate=result.rows[0][0];
-                
+               
                let nowDate=moment(maxdate).format("YYYY-MM-DD HH:mm:ss");
-			    fs.writeFileSync(running.path+'/running.json',JSON.stringify({date:nowDate}));
+			  // console.log(nowDate);
+			    fs.writeFileSync('running.json',JSON.stringify({date:momentToString(convertStandToBeijing(maxdate))}));
 				doRelease(connection,ftp)
             }
         });
@@ -99,20 +107,21 @@ const createJson=(rows,ftp)=>{
            
            let transformJson=removeEmpty(transform.transformToJson(data));
 		   transformJson.meta=clob
-		   let transformXml=transform.transformToXml('weather',transformJson);
-           //let filePath='./upload1/';
+		  // console.log(transformJson)
+           let transformXml=transform.transformToXml('weather',transformJson);
+          // let filePath='./upload1/';
            let fileName=uuidv4()+".xml";
-           //fs.writeFileSync(uploadPath+fileName,transformXml);
+          // fs.writeFileSync(filePath+fileName,transformXml);
            ftp.put(transformXml, fileName, function(err) {
                if (err) {
-                   logger.info({src:clob,err})
+                   logger.debug({src:clob,err})
                };
            });
 		   
 
        }
        catch(err){
-           logger.info({src:clob,err})
+           logger.debug({src:clob,err})
        }
 
    }
@@ -127,10 +136,7 @@ function doRelease(connection,ftp)
                 logger.error(err.message);
         });
     ftp.end();
+	// logger.info(moment().format("YYYY-MM-DD  HH:mm:ss")+' over')
 }
-const transport =(config,running)=>{
-
-}
-module.exports= ftpReady;
-
+ftpReady();
 //console.log(moment().format("YYYY-MM-DD  HH:mm:ss"));
